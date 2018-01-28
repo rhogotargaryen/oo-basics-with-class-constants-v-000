99d4d2bdb747298a3db8820567d3dbdfafe66932
Derek Haller <dhaller.personal@gmail.com>
2018-01-28 09:16:31 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,9 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRANDS << brand
+    if !BRANDS.detect(brand)
+      BRANDS << brand
+    end
   end
   def cobble
     self.condition = "new"
