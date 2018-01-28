fdf061988a6bbdd53067f78d089d52bee128eeed
Derek Haller <dhaller.personal@gmail.com>
2018-01-28 09:04:19 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    if BRANDS.detect?(@brand) == false BRANDS << @brand
+    if BRANDS.detect?(@brand) == false : BRANDS << @brand
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
